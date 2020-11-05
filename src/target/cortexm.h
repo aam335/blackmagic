f9dcdfffd27ac1cb2934ed59e768c266edde1755
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2020-11-05 18:44:04 +0100

    cortexm: Store CPUID.

--- src/target/cortexm.h
+++ src/target/cortexm.h
@@ -171,6 +171,17 @@ extern long cortexm_wait_timeout;
 
 #define	CORTEXM_TOPT_INHIBIT_SRST (1 << 2)
 
+enum cortexm_types {
+	CORTEX_M0  = 0xc200,
+	CORTEX_M0P = 0xc600,
+	CORTEX_M3  = 0xc230,
+	CORTEX_M4  = 0xc240,
+	CORTEX_M7  = 0xc270,
+	CORTEX_M23 = 0xd200,
+	CORTEX_M33 = 0xd210,
+};
+#define CPUID_CORE_MASK 0xfff0
+
 bool cortexm_probe(ADIv5_AP_t *ap);
 ADIv5_AP_t *cortexm_ap(target *t);
 
